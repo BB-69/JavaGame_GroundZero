1556278b923190d0441feeefbdfde777b177a8de
BB-69 <129858054+BB-69@users.noreply.github.com>
2026-02-06 18:58:04 +0700

    Indent

--- src/game/util/Time.java
+++ src/game/util/Time.java
@@ -2,22 +2,22 @@ package game.util;
 
 public final class Time {
 
-    public static float deltaTime;
-    public static final float FIXED_DELTA = 1f / 60f;
+  public static float deltaTime;
+  public static final float FIXED_DELTA = 1f / 60f;
 
-    private static long lastTime;
+  private static long lastTime;
 
-    private Time() {
-    }
+  private Time() {
+  }
 
-    public static void init() {
-        lastTime = System.nanoTime();
-    }
+  public static void init() {
+    lastTime = System.nanoTime();
+  }
 
-    public static float update() {
-        long now = System.nanoTime();
-        deltaTime = (now - lastTime) / 1_000_000_000f;
-        lastTime = now;
-        return deltaTime;
-    }
+  public static float update() {
+    long now = System.nanoTime();
+    deltaTime = (now - lastTime) / 1_000_000_000f;
+    lastTime = now;
+    return deltaTime;
+  }
 }
